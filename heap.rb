2a39aeb340d3f8d8dc5f9fb4a20746146950dd0a
3ricliu <ericliu4943@gmail.com>
2016-04-14 16:25:24 -0700

    creates binary min heap

--- heap.rb
+++ heap.rb
@@ -1,4 +1,4 @@
-# require 'byebug'
+require 'byebug'
 
 class BinaryMinHeap
   def initialize(&prc)
@@ -7,15 +7,27 @@ class BinaryMinHeap
   end
 
   def count
+    @store.length
   end
 
   def extract
+    val = @store.shift
+    @store.unshift(@store.pop)
+    parent_idx = 0
+    length = count
+    @store = BinaryMinHeap.heapify_down(@store, parent_idx, length)
+    val
   end
 
   def peek
+    @store[0]
   end
 
   def push(val)
+    @store << val
+    child_idx = @store.length - 1
+    length = count
+    @store = BinaryMinHeap.heapify_up(@store, child_idx, length)
   end
 
   protected
@@ -35,17 +47,20 @@ class BinaryMinHeap
 
   def self.heapify_down(array, parent_idx, len = array.length, &prc)
     prc ||= Proc.new {|el1, el2| el1 <=> el2}
-    # check children, swap with child that is the smaller one
     children = BinaryMinHeap.child_indices(len, parent_idx)
 
     while children.count != 0
-      if children.count == 1 && array[children.first] < array[parent_idx]
-        array[parent_idx], array[children.first] = array[children.first], array[parent_idx]
-        parent_idx = children.first
-      elsif array[children[0]] < array[children[1]] && array[children[0]] < array[parent_idx]
+      if children.count == 1
+        if prc.call(array[children.first], array[parent_idx]) != 1
+          array[parent_idx], array[children.first] = array[children.first], array[parent_idx]
+          parent_idx = children.first
+        else
+          break
+        end
+      elsif prc.call(array[children[0]], array[children[1]]) != 1 && prc.call(array[children[0]], array[parent_idx]) != 1
         array[parent_idx], array[children[0]] = array[children[0]], array[parent_idx]
         parent_idx = children[0]
-      elsif array[children[0]] > array[children[1]] && array[children[1]] < array[parent_idx]
+      elsif prc.call(array[children[1]], array[children[0]]) != 1 && prc.call(array[children[1]], array[parent_idx]) != 1
         array[parent_idx], array[children[1]] = array[children[1]], array[parent_idx]
         parent_idx = children[1]
       else
@@ -63,7 +78,7 @@ class BinaryMinHeap
     return array if child_idx == 0
     parent_idx = BinaryMinHeap.parent_index(child_idx)
 
-    while prc.call(array[child_idx] <=> array[parent_idx]) != 1
+    while prc.call(array[child_idx],array[parent_idx]) != 1
       array[child_idx], array[parent_idx] = array[parent_idx], array[child_idx]
       child_idx = parent_idx
       return array if child_idx == 0
